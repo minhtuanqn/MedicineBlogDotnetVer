ed7cb87daab1327744a6b320977f9129ee67358e
minhtuanqn <minhtuanqn320@gmail.com>
2022-04-06 02:07:45 +0700

    comment-api-service-layer

---
+++ Business/Dto/CreateCommentDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dto
+{
+    public class CreateCommentDTO
+    {
+        [Required(ErrorMessage = "Content can not be null")]
+        public string content { get; set; }
+
+        public Guid parentId { get; set; }
+
+        [Required(ErrorMessage = "Post id can not be null")]
+        public Guid postId { get; set; }
+
+        [Required(ErrorMessage = "User id can not be null")]
+        public Guid userId { get; set; }
+
+    }
+}
