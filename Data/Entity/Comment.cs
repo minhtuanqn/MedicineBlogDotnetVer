d3f71d106cfc4819ade6a787c1e8826c8972a194
minhtuanqn <minhtuanqn320@gmail.com>
2022-04-05 19:37:35 +0700

    repo-comment-api

---
+++ Data/Entity/Comment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entity
+{
+    [Table("Comment")]
+    public class Comment
+    {
+        [Key]
+        [Column("id")]
+        public Guid id { get; set; }
+
+        [Column("content")]
+        public string content { get; set; }
+
+        [Column("parentId")]
+        public Guid parentId { get; set; }
+
+        [Column("postId")]
+        public Guid postId { get; set; }
+
+        [Column("userId")]
+        public Guid userId { get; set; }
+
+        [Column("createdDate")]
+        public string createdDate { get; set; }
+
+        [Column("Status")]
+        public bool status { get; set; }
+    }
+}
---
+++ Data/Repository/impl/CommentRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using Data.Database;
+using Data.Entity;
+
+namespace Data.Repository.impl
+{
+    public class CommentRepository : GenericRepository<Comment>, ICommentRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public CommentRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
