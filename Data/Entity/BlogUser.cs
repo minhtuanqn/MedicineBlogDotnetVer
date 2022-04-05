7af2f7f90e8efc17a1083759d48cea6b6af008a8
minhtuanqn <minhtuanqn320@gmail.com>
2022-04-06 00:23:00 +0700

    user-repo-layer

---
+++ Data/Entity/BlogUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entity
+{
+    [Table("BlogUser")]
+    public class BlogUser
+    {
+        [Key]
+        [Column("id")]
+        public Guid id { get; set; }
+
+        [Column("email")]
+        public string email { get; set; }
+
+        [Column("phone")]
+        public string phone { get; set; }
+
+        [Column("name")]
+        public string name { get; set; }
+
+        [Column("role")]
+        public string role { get; set; }
+
+        [Column("status")]
+        public bool status { get; set; }
+    }
+}
---
+++ Data/Repository/impl/BlogUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data.Database;
+using Data.Entity;
+
+namespace Data.Repository.impl
+{
+    public class BlogUserRepository : GenericRepository<BlogUser>, IBlogUserRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public BlogUserRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
